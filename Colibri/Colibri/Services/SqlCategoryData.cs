599972295783a02f51a866016aee52920c9b9856
Kajo <ugorcak@gmx.ch>
2018-05-20 17:03:20 +0200

    Modifies Method GetAll

---
+++ Colibri/Colibri/Services/SqlCategoryData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Colibri.Data;
+using Colibri.Models;
+
+namespace Colibri.Services
+{
+    public class SqlCategoryData : ICategoryData
+    {
+        private ColibriDbContext _context;
+
+        public SqlCategoryData(ColibriDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Category> GetAll()
+        {
+            return _context.Categories.OrderBy(c => c.Id);
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
