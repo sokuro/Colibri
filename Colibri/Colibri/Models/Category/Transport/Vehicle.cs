6eb24beef10259dbca926529150bbbc3022c7a79
Kajo <ugorcak@gmx.ch>
2018-05-20 20:09:40 +0200

    Added Main Class Vehicle

---
+++ Colibri/Colibri/Models/Category/Transport/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.Models.Category.Transport
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int ConstructionYear { get; set; }
+        public int CargoVolume { get; set; }
+        public int FuelCapacity { get; set; }
+        public double FuelConsumption { get; set; }
+        public string FuelType { get; set; }
+        public string Color { get; set; }
+        public double Price { get; set; }
+    }
+}
