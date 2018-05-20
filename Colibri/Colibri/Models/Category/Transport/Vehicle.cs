cd8784a9a0327976cfd664b44fe168003f277a08
Kajo <ugorcak@gmx.ch>
2018-05-20 20:14:20 +0200

    created Constructor for the Main Class

--- Colibri/Colibri/Models/Category/Transport/Vehicle.cs
+++ Colibri/Colibri/Models/Category/Transport/Vehicle.cs
@@ -6,17 +6,34 @@ using System.Threading.Tasks;
 
 namespace Colibri.Models.Category.Transport
 {
+    // Main Class Vehicle
     public class Vehicle
     {
+        // Accessors
         public int Id { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
         public int ConstructionYear { get; set; }
-        public int CargoVolume { get; set; }
-        public int FuelCapacity { get; set; }
+        public double CargoVolume { get; set; }
+        public double FuelCapacity { get; set; }
         public double FuelConsumption { get; set; }
         public string FuelType { get; set; }
         public string Color { get; set; }
         public double Price { get; set; }
+
+        // Default Constructor
+        public Vehicle(int Id, string Brand, string Model, int ConstructionYear, double CargoVolume, double FuelCapacity, double FuelConsumption, string FuelType, string Color, double Price)
+        {
+            this.Id = Id;
+            this.Brand = Brand;
+            this.Model = Model;
+            this.ConstructionYear = ConstructionYear;
+            this.CargoVolume = CargoVolume;
+            this.FuelCapacity = FuelCapacity;
+            this.FuelConsumption = FuelConsumption;
+            this.FuelType = FuelType;
+            this.Color = Color;
+            this.Price = Price;
+        }
     }
 }
