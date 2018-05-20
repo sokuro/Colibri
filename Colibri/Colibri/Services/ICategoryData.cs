1e1f886b922ba99034d151b996b773a6e332485c
Kajo <ugorcak@gmx.ch>
2018-05-20 13:07:15 +0200

    CategoryData Interface added

---
+++ Colibri/Colibri/Services/ICategoryData.cs
@@ -0,0 +1,14 @@
+﻿using Colibri.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.Services
+{
+    public interface ICategoryData
+    {
+        IEnumerable<Category> GetAll();
+    }
+}
