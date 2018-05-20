0b6df8aed0075a0eda69e431aec5648900818798
Kajo <ugorcak@gmx.ch>
2018-05-20 11:18:02 +0200

    Adding Routes to the Cotroller

---
+++ Colibri/Colibri/Controller/AboutController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Colibri
+{
+    [Route("about")]
+    public class AboutController : Controller
+    {
+        // Phone
+        [Route("")]
+        public string Phone()
+        {
+            return "031 301 03 47";
+        }
+
+        // Address
+        [Route("address")]
+        public string Address()
+        {
+            return "CH";
+        }
+    }
+}
