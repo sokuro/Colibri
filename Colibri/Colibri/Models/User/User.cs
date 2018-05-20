9a7a2c1db42ea2ce27f6bdfbedc2aa96a5dfab3f
Kajo <ugorcak@gmx.ch>
2018-05-20 19:54:37 +0200

    extended with own DateTime Properties

--- Colibri/Colibri/Models/User/User.cs
+++ Colibri/Colibri/Models/User/User.cs
@@ -7,10 +7,16 @@ using System.Threading.Tasks;
 namespace Colibri.Models.User
 {
     // Class inherites from the Main Person Class
-    public class User : Person
+    class User : Person
     {
-        public User(int Id, string FirstName, string MiddleName, string LastName, string Street, string careOf, int HouseNumber, string City, int Zip, string State, string Country) : base(Id, FirstName, MiddleName, LastName, Street, careOf, HouseNumber, City, Zip, State, Country)
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+
+
+        public User(int Id, string FirstName, string MiddleName, string LastName, string Street, string CareOf, int HouseNumber, string City, int Zip, string State, string Country) : base(Id, FirstName, MiddleName, LastName, Street, CareOf, HouseNumber, City, Zip, State, Country)
         {
         }
+
+        
     }
 }
