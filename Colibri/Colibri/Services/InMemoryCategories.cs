0b5c992b0f994b479d62fd0fa2e2c582f76df2f1
Kajo <ugorcak@gmx.ch>
2018-05-20 13:07:44 +0200

    InMemoryCategories to save the Categories in Memory (temporary Solution) added

---
+++ Colibri/Colibri/Services/InMemoryCategories.cs
@@ -0,0 +1,36 @@
+﻿using Colibri.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.Services
+{
+    // Implement the ICategoryData Interface
+    public class InMemoryCategories : ICategoryData
+    {
+        private List<Category> _categories;
+
+        // Initialize the List
+        // Constructor
+        public InMemoryCategories()
+        {
+            // create some new Categories
+            _categories = new List<Category>
+            {
+                new Category { Id = 1, Name = "Transport" },
+                new Category { Id = 2, Name = "Audio, TV, Video, Foto" },
+                new Category { Id = 3, Name = "Haushalt" },
+                new Category { Id = 4, Name = "Garten" },
+                new Category { Id = 5, Name = "Musik" },
+                new Category { Id = 6, Name = "Sport" }
+            };
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            return _categories.OrderBy(c => c.Name);
+        }
+    }
+}
