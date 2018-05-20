d9cdf48e33b003a7df7213a4c0d4eb2bb06677fa
Kajo <ugorcak@gmx.ch>
2018-05-20 18:19:23 +0200

    Class User that inherites from the Person Class

---
+++ Colibri/Colibri/Models/User/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.Models.User
+{
+    // Class inherites from the Main Person Class
+    public class User : Person
+    {
+        public User(int Id, string FirstName, string MiddleName, string LastName, string Street, string careOf, int HouseNumber, string City, int Zip, string State, string Country) : base(Id, FirstName, MiddleName, LastName, Street, careOf, HouseNumber, City, Zip, State, Country)
+        {
+        }
+    }
+}
