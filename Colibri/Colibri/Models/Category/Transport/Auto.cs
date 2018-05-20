5ab8815a4a7f0a4634397ba3344a62ef26122d2a
Kajo <ugorcak@gmx.ch>
2018-05-20 21:15:05 +0200

    removed the full Path from the Namespace

--- Colibri/Colibri/Models/Category/Transport/Auto.cs
+++ Colibri/Colibri/Models/Category/Transport/Auto.cs
@@ -1,17 +1,12 @@
 ﻿using Colibri.Models.Category.Transport;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Colibri.Models.User;
 
-namespace Colibri.Models.Category.Transport
+namespace Colibri
 {
     public class Auto : Vehicle
     {
         // One-to-One Relationships
-        public User.User UserId { get; set; }
+        public User UserId { get; set; }
 
         public Auto(int Id, string Brand, string Model, int ConstructionYear, double CargoVolume, double FuelCapacity, double FuelConsumption, string FuelType, string Color, double Price) : base(Id, Brand, Model, ConstructionYear, CargoVolume, FuelCapacity, FuelConsumption, FuelType, Color, Price)
         {
