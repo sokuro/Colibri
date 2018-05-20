6af41fa203f35e5beb1b8c8e2513a2b68beb46a9
Kajo <ugorcak@gmx.ch>
2018-05-20 16:45:20 +0200

    Created Constructor to instantialize the DbContext Class to use with different DBs

---
+++ Colibri/Colibri/Data/ColibriDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Colibri.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.Data
+{
+    public class ColibriDbContext : DbContext
+    {
+        public ColibriDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        // Each Entity will need DbSet<T> Property
+        public DbSet<Category> Categories { get; set; }
+    }
+}
