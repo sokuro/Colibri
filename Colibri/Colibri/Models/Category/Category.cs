871e15cb12d844ad2558dfdc091d5728f6084cc6
Kajo <ugorcak@gmx.ch>
2018-05-21 10:28:54 +0200

    Added required Annotations, Annotation to change the Name of the Label

--- Colibri/Colibri/Models/Category/Category.cs
+++ Colibri/Colibri/Models/Category/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ namespace Colibri.Models.Category
     public class Category
     {
         public int Id { get; set; }
+
+        [Display(Name="Category Name")]
+        [Required,MaxLength(50)]
         public string Name { get; set; }
     }
 }
