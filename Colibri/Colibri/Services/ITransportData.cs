54d117e05886114fa52ca900ef8e4c5e3936fee8
Kajo <ugorcak@gmx.ch>
2018-05-23 20:42:16 +0200

    ITransportData Service added

---
+++ Colibri/Colibri/Services/ITransportData.cs
@@ -0,0 +1,19 @@
+﻿using Colibri.Models.Category.Transport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.Services
+{
+    public interface ITransportData
+    {
+        // Method to get all Transports
+        IEnumerable<Transport> GetAll();
+        // Method to get a specific Transport by Id
+        Transport getById(int id);
+        // Method to add a new Transport
+        Transport Add(Transport transport);
+    }
+}
