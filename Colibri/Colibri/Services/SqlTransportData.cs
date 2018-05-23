2fed5fba3267396b69ccb67c3a62de27609daf2a
Kajo <ugorcak@gmx.ch>
2018-05-23 20:55:18 +0200

    SqlTransportData added to store the Context into the DB

---
+++ Colibri/Colibri/Services/SqlTransportData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Colibri.Data;
+using Colibri.Models.Category.Transport;
+
+namespace Colibri.Services
+{
+    public class SqlTransportData : ITransportData
+    {
+        private ColibriDbContext _context;
+
+        public SqlTransportData(ColibriDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Transport> GetAll()
+        {
+            return _context.Transports.OrderBy(t => t.Id);
+        }
+
+        public Transport getById(int id)
+        {
+            return _context.Transports.FirstOrDefault(t => t.Id == id);
+        }
+        public Transport Add(Transport transport)
+        {
+            _context.Transports.Add(transport);
+            _context.SaveChanges();
+            return transport;
+        }
+    }
+}
