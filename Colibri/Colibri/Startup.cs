9ca789543cf6f65c4028055aca1eb5f5a9c5701b
Kajo <ugorcak@gmx.ch>
2018-09-20 20:20:16 +0200

    comment added

--- Colibri/Colibri/Startup.cs
+++ Colibri/Colibri/Startup.cs
@@ -20,6 +20,7 @@ namespace Colibri
             _configuration = configuration;
         }
         // This method gets called by the runtime. Use this method to add services to the container.
+        // Services will be used by DEPENDENCY INJECTION
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
