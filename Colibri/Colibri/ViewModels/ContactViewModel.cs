703b4ac7f793219ff722223c8742f97dddd52b02
Kajo <ugorcak@gmx.ch>
2018-09-23 01:17:33 +0200

    added Validation on the required Properties

--- Colibri/Colibri/ViewModels/ContactViewModel.cs
+++ Colibri/Colibri/ViewModels/ContactViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,16 @@ namespace Colibri.ViewModels
      */
     public class ContactViewModel
     {
+        [Required]
+        [MinLength(5)]
         public string Name { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string Subject { get; set; }
+        [Required]
+        [MaxLength(250)]
         public string Message { get; set; }
     }
 }
