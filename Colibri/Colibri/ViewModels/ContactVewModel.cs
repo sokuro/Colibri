71ad472fb51121d5f609717bfda7f9e29ec89be9
Kajo <ugorcak@gmx.ch>
2018-09-23 00:48:15 +0200

    added ContactViewModel to handle the Contact Form Data

---
+++ Colibri/Colibri/ViewModels/ContactVewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.ViewModels
+{
+    /*
+     * ViewModel class to handle the Data in the Contact Form
+     */
+    public class ContactVewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
