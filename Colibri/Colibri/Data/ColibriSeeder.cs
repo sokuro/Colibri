754478a1b5a9091d423504c18b87ab1e1bd93712
Kajo <ugorcak@gmx.ch>
2018-10-09 22:00:25 +0200

    added Seeder to seed Data into the DB

---
+++ Colibri/Colibri/Data/ColibriSeeder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.Data
+{
+    // Db Seeder
+    public class ColibriSeeder
+    {
+        private readonly ColibriDbContext _context;
+
+        public ColibriSeeder(ColibriDbContext context)
+        {
+            _context = context;
+        }
+
+        // Method to seed Data
+        public void Seed()
+        {
+            _context.Database.EnsureCreated();
+        }
+    }
+}
