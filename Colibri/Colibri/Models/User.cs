35e0c264f9bbf62816367b3bb0ef5b362618e474
Kajo <ugorcak@gmx.ch>
2018-10-09 21:09:09 +0200

    reengineered: changed the  Person Entity to User Entity, removed the Inheritance and the old User Entity

--- Colibri/Colibri/Models/User.cs
+++ Colibri/Colibri/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,15 +7,25 @@ using System.Threading.Tasks;
 
 namespace Colibri.Models.User
 {
-    // Class inherites from the Main Person Class
-    public class User : Person
+    // Identity User
+    public class User : IdentityUser
     {
-        public DateTime Created { get; set; }
-        public DateTime Modified { get; set; }
+        // Accessors
+        //public int Id { get; set; }
+        public string FirstName { get; set; }
+        //public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        //public string Street { get; set; }
+        //public string CareOf { get; set; }
+        //public int HouseNumber { get; set; }
+        //public string City { get; set; }
+        //public int Zip { get; set; }
+        //public string State { get; set; }
+        //public string Country { get; set; }
+        public string Password { get; set; }
 
-
-        public User(int Id, string FirstName, string MiddleName, string LastName, string Street, string CareOf, int HouseNumber, string City, int Zip, string State, string Country) : base(Id, FirstName, MiddleName, LastName, Street, CareOf, HouseNumber, City, Zip, State, Country)
-        {
-        }
+        // Logging
+        //public DateTime Created { get; set; }
+        //public DateTime Modified { get; set; }
     }
 }
