6096484ef3079820589a15bdfe79a36e3de2df81
Kajo <ugorcak@gmx.ch>
2018-10-10 22:11:31 +0200

    added OrderItem Model

--- Colibri/Colibri/Models/Order.cs
+++ Colibri/Colibri/Models/Order.cs
@@ -12,8 +12,13 @@ namespace Colibri.Models
     {
         [Key]
         public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string OrderNumber { get; set; }
 
         //ICollection<Product> Products { get; set; }
+
+        // Collection allows a relation to another Entity (-> OrderItems)
+        public ICollection<OrderItem> Items { get; set; }
 
         // allow the User to make an Order
         public User OrderUser { get; set; }
---
+++ Colibri/Colibri/Models/OrderItem.cs
@@ -0,0 +1,20 @@
+﻿using Colibri.Models.Category;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        // relationship to the Entity 'product'
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        // relationship to the Entity 'order'
+        public Order Order { get; set; }
+    }
+}
