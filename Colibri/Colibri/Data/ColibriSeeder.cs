f0a988d1424f38b6bf4cda470aaea0c2d6782310
Kajo <ugorcak@gmx.ch>
2018-10-10 21:58:27 +0200

    seeded Categories

--- Colibri/Colibri/Data/ColibriSeeder.cs
+++ Colibri/Colibri/Data/ColibriSeeder.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Colibri.Models.Category;
+using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +14,36 @@ namespace Colibri.Data
     public class ColibriSeeder
     {
         private readonly ColibriDbContext _context;
+        private readonly IHostingEnvironment _hosting;
 
-        public ColibriSeeder(ColibriDbContext context)
+        public ColibriSeeder(ColibriDbContext context, IHostingEnvironment hosting)
         {
             _context = context;
+            _hosting = hosting;
         }
 
         // Method to seed Data
         public void Seed()
         {
             _context.Database.EnsureCreated();
+
+            // check: data in DB?
+            if (!_context.Categories.Any())
+            {
+                // create some sample data
+                // get the json data (art.json)
+                var filepath = Path.Combine(_hosting.ContentRootPath, "Data/art.json");
+                var json = File.ReadAllText(filepath);
+
+                // add categories
+                // use Deserialization
+                var categories = JsonConvert.DeserializeObject<IEnumerable<Category>>(json);
+                // addRange to cut the Collection (large!)
+                _context.Categories.AddRange(categories);
+
+                // save changes
+                _context.SaveChanges();
+            }
         }
     }
 }
