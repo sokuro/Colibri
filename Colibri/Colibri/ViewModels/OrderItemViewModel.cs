0cb1283672a1c4f85d140eec29832bffafb7b978
Kajo <ugorcak@gmx.ch>
2018-10-14 00:51:52 +0200

    added OrderViewModel

---
+++ Colibri/Colibri/ViewModels/OrderItemViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.ViewModels
+{
+    public class OrderItemViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public decimal UnitPrice { get; set; }
+
+        // extend the Properties of the individual Order's Item
+        [Required]
+        public int ProductId { get; set; }
+
+        //public string ProductCategory { get; set; }
+    }
+
+}
--- Colibri/Colibri/ViewModels/OrderViewModel.cs
+++ Colibri/Colibri/ViewModels/OrderViewModel.cs
@@ -1,6 +1,18 @@
-﻿namespace Colibri.ViewModels
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Colibri.ViewModels
 {
     public class OrderViewModel
     {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        [Required]
+        [MinLength(4)]
+        public string OrderNumber { get; set; }
+
+        // to allow returning of the Data Collection
+        public ICollection<OrderItemViewModel> Items { get; set; }
     }
 }
