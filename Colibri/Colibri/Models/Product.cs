4773c92242b65d6972d04025f5b1e0f3a366452c
Kajo <ugorcak@gmx.ch>
2018-10-13 19:39:52 +0200

    changed the DataType of the Product's Price to Decimal

--- Colibri/Colibri/Models/Product.cs
+++ Colibri/Colibri/Models/Product.cs
@@ -13,7 +13,7 @@ namespace Colibri.Models
         [Key]
         public int ProductId { get; set; }
         public string Name { get; set; }
-        public double Price { get; set; }
+        public decimal Price { get; set; }
         public bool Available { get; set; }
         public string Image { get; set; }
         public string Description { get; set; }
