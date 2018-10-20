5a56d36b82bd5e3b95fbc7c794a6539be03906ea
Kajo <ugorcak@gmx.ch>
2018-10-21 01:10:52 +0200

    extended the CategoryTypesController with the Create Action Method

--- Colibri/Colibri/Controllers/CategoryTypesController.cs
+++ Colibri/Colibri/Controllers/CategoryTypesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Colibri.Data;
+using Colibri.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Colibri.Controllers
@@ -18,6 +19,30 @@ namespace Colibri.Controllers
         public IActionResult Index()
         {
             return View(_db.CategoryTypes.ToList());
+        }
+
+        // Post: /<controller>/Create
+        // @param Category
+        [HttpPost("create")]
+        //[Authorize]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CategoryTypes categoryTypes)
+        {
+            // Check the State Model Binding
+            if (ModelState.IsValid)
+            {
+                _db.Add(categoryTypes);
+                await _db.SaveChangesAsync();
+
+                // avoid Refreshing the POST Operation -> Redirect
+                //return View("Details", newCategory);
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                // one can simply return to the Form View again for Correction
+                return View(categoryTypes);
+            }
         }
     }
 }
