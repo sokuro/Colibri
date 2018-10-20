97df1ac6b496905907b4f87b1a690dd56069f9d7
Kajo <ugorcak@gmx.ch>
2018-10-20 17:45:31 +0200

    added ProductViewModel

---
+++ Colibri/Colibri/ViewModels/ProductViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Colibri.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.ViewModels
+{
+    public class ProductViewModel
+    {
+        public Product Product { get; set; }
+
+    }
+}
