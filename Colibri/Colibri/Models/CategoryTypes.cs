cdd896fc19156ba5b18e9e897daaceb2c86d0fc0
Kajo <ugorcak@gmx.ch>
2018-10-21 02:01:27 +0200

    removed the Required Annotation from the ID

--- Colibri/Colibri/Models/CategoryTypes.cs
+++ Colibri/Colibri/Models/CategoryTypes.cs
@@ -9,7 +9,6 @@ namespace Colibri.Models
 {
     public class CategoryTypes
     {
-        [Required]
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
