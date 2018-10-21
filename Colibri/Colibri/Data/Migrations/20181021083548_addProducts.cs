acc6a433377b868ca865036803f680f00469c088
Kajo <ugorcak@gmx.ch>
2018-10-21 10:36:56 +0200

    updated Migrations with the Products

---
+++ Colibri/Colibri/Data/Migrations/20181021083548_addProducts.cs
@@ -0,0 +1,233 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Colibri.Migrations
+{
+    public partial class addProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItem_Products_ProductId",
+                table: "OrderItem");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoriesCategoryId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_AspNetUsers_UserId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_CategoriesCategoryId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_UserId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CategoriesCategoryId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductId",
+                table: "Products",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryTypeId",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SpecialTagId",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Available = table.Column<bool>(nullable: false),
+                    Image = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    OfferOrder = table.Column<bool>(nullable: false),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.ProductId);
+                    table.ForeignKey(
+                        name: "FK_Product_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryTypeId",
+                table: "Products",
+                column: "CategoryTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_SpecialTagId",
+                table: "Products",
+                column: "SpecialTagId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_UserId",
+                table: "Product",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItem_Product_ProductId",
+                table: "OrderItem",
+                column: "ProductId",
+                principalTable: "Product",
+                principalColumn: "ProductId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_CategoryTypes_CategoryTypeId",
+                table: "Products",
+                column: "CategoryTypeId",
+                principalTable: "CategoryTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_SpecialTags_SpecialTagId",
+                table: "Products",
+                column: "SpecialTagId",
+                principalTable: "SpecialTags",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItem_Product_ProductId",
+                table: "OrderItem");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_CategoryTypes_CategoryTypeId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_SpecialTags_SpecialTagId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_CategoryTypeId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_SpecialTagId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryTypeId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "SpecialTagId",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Products",
+                newName: "ProductId");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(double));
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoriesCategoryId",
+                table: "Products",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Products",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoriesCategoryId",
+                table: "Products",
+                column: "CategoriesCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_UserId",
+                table: "Products",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItem_Products_ProductId",
+                table: "OrderItem",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "ProductId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoriesCategoryId",
+                table: "Products",
+                column: "CategoriesCategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_AspNetUsers_UserId",
+                table: "Products",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
