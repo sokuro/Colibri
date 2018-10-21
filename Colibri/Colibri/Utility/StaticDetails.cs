490758edcb4b48a41e20c35f18ac9ba577b6f830
Kajo <ugorcak@gmx.ch>
2018-10-21 11:48:04 +0200

    added static Path Handler, Image for the Product

---
+++ Colibri/Colibri/Utility/StaticDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.Utility
+{
+    // static Paths
+    public class StaticDetails
+    {
+        public const string DefaultProductImage = "default_product.jpg";
+        public const string ImageFolder = @"image\ProductImage";
+    }
+}
