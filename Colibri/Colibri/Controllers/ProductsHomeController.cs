78c00aa5cd193f04a218efd3ad5cb7a03fa26760
Kajo <ugorcak@gmx.ch>
2018-10-26 11:51:38 +0200

    adding Repository Handling for the Products

---
+++ Colibri/Colibri/Controllers/ProductsHomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Colibri.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Colibri.Controllers
+{
+    public class ProductsHomeController : Controller
+    {
+        private readonly IColibriRepository _repository;
+
+        public ProductsHomeController(IColibriRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Colibri/Colibri/Services/IProductsData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.Services
+{
+    /*
+     * Interface Controller to handle the DB
+     */
+    public interface IProductsData
+    {
+        
+    }
+}
