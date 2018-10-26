59d43bc6fb26c5ba2add9e7f5909d9440b349f27
Kajo <ugorcak@gmx.ch>
2018-10-27 00:10:26 +0200

    added ProductsHome Overview: Controller, ViewModel, View, NavigationReference

--- Colibri/Colibri/Controllers/ProductsHomeController.cs
+++ Colibri/Colibri/Controllers/ProductsHomeController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Colibri.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Colibri.Controllers
 {
@@ -14,16 +15,23 @@ namespace Colibri.Controllers
     //[Authorize(Roles = "Administrator, PowerUser")]
     public class ProductsHomeController : Controller
     {
-        private readonly IColibriRepository _repository;
+        //private readonly IColibriRepository _repository;
+        private readonly ColibriDbContext _colibriDbContext;
 
-        public ProductsHomeController(IColibriRepository repository)
+        //public ProductsHomeController(IColibriRepository repository)
+        public ProductsHomeController(ColibriDbContext colibriDbContext)
         {
-            _repository = repository;
+            //_repository = repository;
+            _colibriDbContext = colibriDbContext;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var productList = _repository.GetAllProductsAsync();
+            //var productList = _repository.GetAllProductsAsync();
+            var productList = await _colibriDbContext.Products
+                    .Include(p => p.CategoryTypes)
+                    .Include(p => p.SpecialTags)
+                    .ToListAsync();
 
             return View(productList);
         }
---
+++ Colibri/Colibri/ViewModels/ProductsHomeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Colibri.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.ViewModels
+{
+    public class ProductsHomeViewModel
+    {
+        public Products Products { get; set; }
+
+        public IEnumerable<CategoryTypes> CategoryTypes { get; set; }
+        public IEnumerable<SpecialTags> SpecialTags { get; set; }
+
+    }
+}
