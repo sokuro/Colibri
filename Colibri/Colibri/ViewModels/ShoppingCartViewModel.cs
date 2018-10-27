af30426bd727a025feb3054af82d9ba4ead721fa
Kajo <ugorcak@gmx.ch>
2018-10-27 18:14:55 +0200

    added Shopping Cart ViewModel

---
+++ Colibri/Colibri/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Colibri.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.ViewModels
+{
+    /*
+     * Shopping Cart View Model with:
+     * #1: Products
+     * #2: Appointments
+     */
+    public class ShoppingCartViewModel
+    {
+        public List<Products> Products { get; set; }
+        public Appointments Appointments { get; set; }
+    }
+}
