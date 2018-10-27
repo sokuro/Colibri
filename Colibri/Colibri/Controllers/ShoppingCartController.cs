76aeb6e9cd709f082f8869ed0da3e7e2fd430817
Kajo <ugorcak@gmx.ch>
2018-10-27 18:48:48 +0200

    extended the ShoppingCartController's Index Method: retrieve the Products from the Session

--- Colibri/Colibri/Controllers/ShoppingCartController.cs
+++ Colibri/Colibri/Controllers/ShoppingCartController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Colibri.Data;
+using Colibri.Extensions;
+using Colibri.Models;
 using Colibri.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -27,8 +29,25 @@ namespace Colibri.Controllers
             };
         }
 
+        // Get Index ShoppingCart
+        // retrieve all the Products from the Session
         public IActionResult Index()
         {
+            // check first, if anything exists in the Session
+            // Session Name : "ssSessionOrderExists"
+            List<int> lstSessionOrderExists = HttpContext.Session.Get<List<int>>("ssSessionOrderExists");
+
+            if (lstSessionOrderExists.Count > 0)
+            {
+                foreach (int cartItem in lstSessionOrderExists)
+                {
+                    
+                    Products products = _colibriDbContext.Products
+                        .Where(p => p.Id == cartItem)
+                        .FirstOrDefault();
+                }
+            }
+
             return View();
         }
     }
