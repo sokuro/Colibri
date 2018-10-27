3ef668603255b3db091212095642c6ccd9b7501c
Kajo <ugorcak@gmx.ch>
2018-10-27 18:41:29 +0200

    added ShoppingCart Controller with a Constructor. Binding ShoppingCart ViewModel

---
+++ Colibri/Colibri/Controllers/ShoppingCartController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Colibri.Data;
+using Colibri.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Colibri.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly ColibriDbContext _colibriDbContext;
+
+        // bind the ShoppingCartViewModel
+        [BindProperty]
+        public ShoppingCartViewModel ShoppingCartViewModel { get; set; }
+
+        public ShoppingCartController(ColibriDbContext colibriDbContext)
+        {
+            _colibriDbContext = colibriDbContext;
+
+            // initialize the ShoppingCartViewModel
+            ShoppingCartViewModel = new ShoppingCartViewModel()
+            {
+                Products = new List<Models.Products>()
+            };
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
