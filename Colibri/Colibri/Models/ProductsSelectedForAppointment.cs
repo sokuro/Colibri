8b15860e73ba24c9feb99f620a899917ddf0fb80
Kajo <ugorcak@gmx.ch>
2018-10-27 18:02:27 +0200

    added ProductsSelectedForAppointments as Entity for Connection between the Products and the User's, resp. Order's Appointment

---
+++ Colibri/Colibri/Models/ProductsSelectedForAppointment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.Models
+{
+    /*
+     * Entity as Connector between the Products and the User, resp. the Order
+     */
+    public class ProductsSelectedForAppointment
+    {
+        public int Id { get; set; }
+
+        public int AppointmentId { get; set; }
+        [ForeignKey("AppointmentId")]
+        public virtual Appointments Appointments { get; set; }
+
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public virtual Products Products { get; set; }
+    }
+}
