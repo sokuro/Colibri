6cf34e1d9fed6512b04dab44f341ac4c4782511d
Kajo <ugorcak@gmx.ch>
2018-10-28 01:37:28 +0200

    added the Index POST Action Method to create and save an Appointment into the DB

--- Colibri/Colibri/Controllers/ShoppingCartController.cs
+++ Colibri/Colibri/Controllers/ShoppingCartController.cs
@@ -6,6 +6,7 @@ using Colibri.Data;
 using Colibri.Extensions;
 using Colibri.Models;
 using Colibri.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -57,6 +58,56 @@ namespace Colibri.Controllers
             
             // pass the ShoppingCartViewModel to the View
             return View(ShoppingCartViewModel);
+        }
+
+        // POST: Index
+        // create an Appointment
+        [HttpPost, ActionName("Index")]
+        [ValidateAntiForgeryToken]
+        //[Authorize]
+        public IActionResult IndexPost()
+        {
+            // check first, if anything exists in the Session
+            // Session Name : "ssSessionOrderExists"
+            List<int> lstSessionOrderExists = HttpContext.Session.Get<List<int>>("ssSessionOrderExists");
+
+            // merge (add) the Appointment Date and Time to the Appointment Date itself
+            ShoppingCartViewModel.Appointments.AppointmentDate = ShoppingCartViewModel.Appointments.AppointmentDate
+                                                                    .AddHours(ShoppingCartViewModel.Appointments.AppointmentTime.Hour)
+                                                                    .AddMinutes (ShoppingCartViewModel.Appointments.AppointmentTime.Minute);
+
+            // create an Object for the Appointments
+            Appointments appointments = ShoppingCartViewModel.Appointments;
+
+            // add the Appointments to the DB
+            _colibriDbContext.Appointments.Add(appointments);
+            _colibriDbContext.SaveChanges();
+
+            // by saving one gets the Appointment Id that has been just created
+            int appointmentId = appointments.Id;
+
+            // this created Id can be used to insert Records inside the selected Products
+            foreach (int productId in lstSessionOrderExists)
+            {
+                // everytime a new Object will be created
+                ProductsSelectedForAppointment productsSelectedForAppointment = new ProductsSelectedForAppointment()
+                {
+                    AppointmentId = appointmentId,
+                    ProductId = productId
+                };
+
+                // add to the DB
+                _colibriDbContext.ProductsSelectedForAppointment.Add(productsSelectedForAppointment);
+            }
+            // save the Changes all together after the Iteration
+            _colibriDbContext.SaveChanges();
+
+            // After adding the Items to the DB, empty the Cart (by creating a new Session)
+            lstSessionOrderExists = new List<int>();
+            HttpContext.Session.Set("ssSessionOrderExists", lstSessionOrderExists);
+
+            // redirect to Action
+            return RedirectToAction(nameof(Index));
         }
     }
 }
