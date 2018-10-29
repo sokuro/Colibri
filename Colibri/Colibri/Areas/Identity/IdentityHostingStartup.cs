168782024f1ae7692cd5848a4cdfc64ba7e34762
Kajo <ugorcak@gmx.ch>
2018-10-30 00:23:51 +0100

    added Dotnet Core 2.1 Identity as Area / Razor Pages

---
+++ Colibri/Colibri/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Colibri.Data;
+using Colibri.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Colibri.Areas.Identity.IdentityHostingStartup))]
+namespace Colibri.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ Colibri/Colibri/Models/PagingInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.Models
+{
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }
+
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        public int totalPage => (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
+
+        //This will be used to build URL
+        public string urlParam { get; set; }
+    }
+}
