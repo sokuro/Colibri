ae5b4a83ae7aede9c8d4ff26a429a0e1df94c101
Kajo <ugorcak@gmx.ch>
2018-11-01 00:03:51 +0100

    hidden the Register Navigation Element. Authorized the Register RazorPageView

--- Colibri/Colibri/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Colibri/Colibri/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -14,7 +14,7 @@ using Colibri.Utility;
 
 namespace Colibri.Areas.Identity.Pages.Account
 {
-    [AllowAnonymous]
+    [Authorize(Roles = StaticDetails.SuperAdminEndUser)]
     public class RegisterModel : PageModel
     {
         private readonly SignInManager<IdentityUser> _signInManager;
