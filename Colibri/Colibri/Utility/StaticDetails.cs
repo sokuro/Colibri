4665beb69e0728865f6ee3535941067d7e9c2261
Kajo <ugorcak@gmx.ch>
2018-10-31 21:36:17 +0100

    extend Static Details with the Admin and SuperAdmin Constants

--- Colibri/Colibri/Utility/StaticDetails.cs
+++ Colibri/Colibri/Utility/StaticDetails.cs
@@ -9,7 +9,12 @@ namespace Colibri.Utility
     // static Paths
     public class StaticDetails
     {
+        // Image Folder
         public const string DefaultProductImage = "default_product.jpg";
         public const string ImageFolder = @"img\ProductImage";
+
+        // User Roles
+        public const string AdminEndUser = "Admin";
+        public const string SuperAdminEndUser = "Super Admin";
     }
 }
