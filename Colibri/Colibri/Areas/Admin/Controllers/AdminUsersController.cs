07e48b7c134ea53fce2848825f7d5a7f30fa269a
Kajo <ugorcak@gmx.ch>
2018-10-31 22:05:19 +0100

    added AdminUsersController to manage Application Users outside the DB directly in the View

---
+++ Colibri/Colibri/Areas/Admin/Controllers/AdminUsersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Colibri.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Colibri.Areas.Admin.Controllers
+{
+    /*
+     * Controller to manage Application Users outside the DB
+     */
+    [Area("Admin")]
+    public class AdminUsersController : Controller
+    {
+        private readonly ColibriDbContext _colibriDbContext;
+
+        public AdminUsersController(ColibriDbContext colibriDbContext)
+        {
+            _colibriDbContext = colibriDbContext;
+        }
+        public IActionResult Index()
+        {
+            // return the List of the Application Users
+            var listOfAppUsers = _colibriDbContext.ApplicationUsers.ToList();
+
+            return View(listOfAppUsers);
+        }
+    }
+}
