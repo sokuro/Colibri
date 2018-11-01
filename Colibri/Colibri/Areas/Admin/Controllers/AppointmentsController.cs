7dbb332062725ff8ac8c83d7bea289946d1c427e
Kajo <ugorcak@gmx.ch>
2018-11-01 22:16:52 +0100

    added Appointment Controller

---
+++ Colibri/Colibri/Areas/Admin/Controllers/AppointmentsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Colibri.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Colibri.Areas.Admin.Controllers
+{
+    /*
+     * Controller to handle the User Appointments
+     * 
+     * Authorize both the Admin and SuperAdmin
+     */
+    [Authorize(Roles = StaticDetails.AdminEndUser + "," + StaticDetails.SuperAdminEndUser)]
+    [Area("Admin")]
+    public class AppointmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
