984f9718a89a1a8ea1c1d343c6a274af4f579ca2
Kajo <ugorcak@gmx.ch>
2018-11-02 00:37:28 +0100

    extended the Appointment Controller with the Possibility to return the Appointments for the specific EndUser

--- Colibri/Colibri/Areas/Admin/Controllers/AppointmentsController.cs
+++ Colibri/Colibri/Areas/Admin/Controllers/AppointmentsController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Colibri.Data;
 using Colibri.Utility;
+using Colibri.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Colibri.Areas.Admin.Controllers
 {
@@ -17,9 +21,44 @@ namespace Colibri.Areas.Admin.Controllers
     [Area("Admin")]
     public class AppointmentsController : Controller
     {
-        public IActionResult Index()
+        private readonly ColibriDbContext _colibriDbContext;
+
+        public AppointmentsController(ColibriDbContext colibriDbContext)
         {
-            return View();
+            _colibriDbContext = colibriDbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Security Claims
+            System.Security.Claims.ClaimsPrincipal currentUser = this.User;
+
+            // Claims Identity
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            // only if the User is AdminUser -> get ID
+            AppointmentViewModel appointmentViewModel = new AppointmentViewModel
+            {
+                // initialize
+                Appointments = new List<Models.Appointments>()
+            };
+
+            // include the Appointments Person (populate it)
+            appointmentViewModel.Appointments = _colibriDbContext.Appointments
+                                                    .Include(a => a.AppPerson).ToList();
+
+            // check if the User is Admin or SuperAdmin
+            if (User.IsInRole(StaticDetails.AdminEndUser))
+            {
+                // AdminEndUser sees only Appoints addressed to them!!!
+                appointmentViewModel.Appointments = appointmentViewModel.Appointments
+                                                        .Where(a => a.AppPersonId == claim.Value)
+                                                        .ToList();
+            }
+
+            // return the View Model for the Appointments
+            return View(appointmentViewModel);
         }
     }
 }
