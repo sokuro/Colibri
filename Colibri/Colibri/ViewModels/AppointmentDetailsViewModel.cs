8aa76b650f4521b3f93d49c5e116f384ea19c10b
ugorcak <ugorcak@gmx.ch>
2018-11-03 17:23:52 +0100

    added ViewModel to handle Appointment Details

---
+++ Colibri/Colibri/ViewModels/AppointmentDetailsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Colibri.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.ViewModels
+{
+    /*
+     * ViewModel to handle Appointment Details
+     */
+    public class AppointmentDetailsViewModel
+    {
+        public Appointments Appointment { get; set; }
+        public List<ApplicationUser> AppPerson { get; set; }
+        public List<Products> Products { get; set; }
+    }
+}
