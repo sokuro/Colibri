581bc987193ad668b055beda93f456f0413fe062
ugorcak <ugorcak@gmx.ch>
2018-11-04 17:48:32 +0100

    extende Extension to convert a List Elem to IEnumerable for the AppPerson, where the ID is a String instead of an Integer

--- Colibri/Colibri/Extensions/IEnumerableExtensions.cs
+++ Colibri/Colibri/Extensions/IEnumerableExtensions.cs
@@ -27,5 +27,27 @@ namespace Colibri.Extensions
                        Selected = item.GetPropertyValue("Id").Equals(selectedValue.ToString()),
                    };
         }
+
+        // because the AppPerson has EMail as ID, implemented Method to handle Strings
+        // 1st Argument <IEnumerable>
+        // 2nd Argument as String
+        public static IEnumerable<SelectListItem> ToSelectListItemString<T>(this IEnumerable<T> items, string selectedValue)
+        {
+            if (selectedValue == null)
+            {
+                // build an empty String
+                selectedValue = "";
+            }
+
+            // Linq used
+            return from item in items
+                   select new SelectListItem
+                   {
+                       // to retrieve the Name Property from the Collection -> another ExtMethod: 'ReflectionExtension'
+                       Text = item.GetPropertyValue("Name"),
+                       Value = item.GetPropertyValue("Id"),
+                       Selected = item.GetPropertyValue("Id").Equals(selectedValue.ToString()),
+                   };
+        }
     }
 }
