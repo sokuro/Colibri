d378fdf49a70415f48d8f8dbac11db738d476ccc
ugorcak <ugorcak@gmx.ch>
2018-11-09 08:51:24 +0100

    added Class to fetch the secure Email Key

---
+++ Colibri/Colibri/Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.Services
+{
+    /*
+     * Class to fetch the secure Email Key
+     */
+    public class AuthMessageSenderOptions
+    {
+        public string SendGridUser { get; set; }
+        public string SendGridKey { get; set; }
+    }
+}
