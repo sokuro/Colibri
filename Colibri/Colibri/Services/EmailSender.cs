1118646a63591ef93314b2029f4e601f9b69e937
ugorcak <ugorcak@gmx.ch>
2018-11-09 08:56:17 +0100

    added EmailSender Class using the SendGrid Library

---
+++ Colibri/Colibri/Services/EmailSender.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.Services
+{
+    /*
+     * SendGrid Email Service
+     */
+    public class EmailSender
+    {
+        public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor)
+        {
+            Options = optionsAccessor.Value;
+        }
+
+        public AuthMessageSenderOptions Options { get; } //set only via Secret Manager
+
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            // get the Environment Secret Variable
+            var apiKey = Environment.GetEnvironmentVariable("ENV_VARIABLE_COLIBRI");
+            //return Execute(Options.SendGridKey, subject, message, email);
+            return Execute(apiKey, subject, message, email);
+        }
+
+        public Task Execute(string apiKey, string subject, string message, string email)
+        {
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("sokuro@yacrol.com", "Sokuro Matsuda"),
+                Subject = subject,
+                PlainTextContent = message,
+                HtmlContent = message
+            };
+            msg.AddTo(new EmailAddress(email));
+
+            // Disable click tracking.
+            // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
+            msg.SetClickTracking(false, false);
+
+            return client.SendEmailAsync(msg);
+        }
+    }
+}
