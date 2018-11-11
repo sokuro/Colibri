e80b4c68672fc74be41993f8baceb5c9947201b5
ugorcak <ugorcak@gmx.ch>
2018-11-11 13:47:49 +0100

    added ApplicationUserController to handle the Application Users

---
+++ Colibri/Colibri/Areas/Customer/Controllers/ApplicationUserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Colibri.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Colibri.Areas.Customer.Controllers
+{
+    /*
+     * Controller to manage Application Users
+     * 
+     * authorize for only for the registered Users
+     */
+    [Authorize(Roles = StaticDetails.AdminEndUser)]
+    [Area("Customer")]
+    public class ApplicationUserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
