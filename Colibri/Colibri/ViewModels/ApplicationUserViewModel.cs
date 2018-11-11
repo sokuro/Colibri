23aae55c9874ea344ac128a236cfbd0574299399
ugorcak <ugorcak@gmx.ch>
2018-11-11 15:21:50 +0100

    added ApplicationUser View Model

---
+++ Colibri/Colibri/ViewModels/ApplicationUserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Colibri.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.ViewModels
+{
+    /*
+     * ViewModel to handle the Application Users
+     */
+    public class ApplicationUserViewModel
+    {
+        // List of Users
+        public List<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
