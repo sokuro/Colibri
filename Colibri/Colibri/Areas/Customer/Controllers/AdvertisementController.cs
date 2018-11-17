2ca25b383ea6d4cc46b5c0529434104ee38fc8b1
ugorcak <ugorcak@gmx.ch>
2018-11-17 17:35:07 +0100

    added AdvertisementController

---
+++ Colibri/Colibri/Areas/Customer/Controllers/AdvertisementController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Colibri.Data;
+using Colibri.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Colibri.Areas.Customer.Controllers
+{
+    /*
+     * Controller for the Advertisement View
+     * 
+     * authorize only the AdminEndUser (registered User)
+     */
+    [Authorize(Roles = StaticDetails.AdminEndUser + "," + StaticDetails.SuperAdminEndUser)]
+    [Area("Customer")]
+    public class AdvertisementController : Controller
+    {
+        private readonly ColibriDbContext _colibriDbContext;
+
+        public AdvertisementController(ColibriDbContext colibriDbContext)
+        {
+            _colibriDbContext = colibriDbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var productList = await _colibriDbContext.Products
+                    .Include(p => p.CategoryTypes)
+                    .Include(p => p.SpecialTags)
+                    .ToListAsync();
+
+            return View(productList);
+        }
+    }
+}
