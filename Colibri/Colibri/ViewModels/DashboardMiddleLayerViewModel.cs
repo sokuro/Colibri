3ea1941e31d90de30d4696a960bb7c4f8b99d138
ugorcak <ugorcak@gmx.ch>
2018-11-18 13:56:40 +0100

    added Dashboard-MiddleLayer between the Main Dashboard and the Adding/Searching of Products/Services Functionality. Added Controller, View, ViewModel and References to them from the Main Pages

---
+++ Colibri/Colibri/ViewModels/DashboardMiddleLayerViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Colibri.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.ViewModels
+{
+    /*
+     * Dashboard Middle Layer with:
+     * 
+     * #1: Categories
+     * #2: Products
+     */
+    public class DashboardMiddleLayerViewModel
+    {
+        public List<CategoryTypes> Categories { get; set; }
+        public List<Products> Products { get; set; }
+    }
+}
