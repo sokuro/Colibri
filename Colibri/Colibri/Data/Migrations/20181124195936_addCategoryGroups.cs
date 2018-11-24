ccadcb1dd821a3149ab11ef7b3427c8867116bb0
ugorcak <ugorcak@gmx.ch>
2018-11-24 21:47:51 +0100

    added CategoryGroups Entity, created Migration, updated DB

---
+++ Colibri/Colibri/Data/Migrations/20181124195936_addCategoryGroups.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Colibri.Migrations
+{
+    public partial class addCategoryGroups : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryGroupId",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "CategoryGroups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryGroups", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryGroupId",
+                table: "Products",
+                column: "CategoryGroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_CategoryGroups_CategoryGroupId",
+                table: "Products",
+                column: "CategoryGroupId",
+                principalTable: "CategoryGroups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_CategoryGroups_CategoryGroupId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "CategoryGroups");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_CategoryGroupId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryGroupId",
+                table: "Products");
+        }
+    }
+}
