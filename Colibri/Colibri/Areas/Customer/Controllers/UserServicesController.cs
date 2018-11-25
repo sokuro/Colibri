742a1816d50a014f834dc21d838a4145de40630b
ugorcak <ugorcak@gmx.ch>
2018-11-25 19:12:31 +0100

    added UserServices Controller

---
+++ Colibri/Colibri/Areas/Customer/Controllers/UserServicesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Colibri.Data;
+using Colibri.Utility;
+using Colibri.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace Colibri.Areas.Customer.Controllers
+{
+    /*
+     * Controller for the User Services View
+     *
+     * authorize only the AdminEndUser (registered User)
+     */
+    //[Authorize(Roles = StaticDetails.AdminEndUser + "," + StaticDetails.SuperAdminEndUser)]
+    [Area("Customer")]
+    public class UserServicesController : Controller
+    {
+        private readonly ColibriDbContext _colibriDbContext;
+        private readonly HostingEnvironment _hostingEnvironment;
+        private readonly IStringLocalizer<ProductsHomeController> _localizer;
+
+        // bind to the UserServices ViewModel
+        // not necessary to create new Objects
+        // allowed to use the ViewModel without passing it as ActionMethod Parameter
+        [BindProperty]
+        public UserServicesViewModel UserServicesViewModel { get; set; }
+
+        public UserServicesController(ColibriDbContext colibriDbContext, 
+            HostingEnvironment hostingEnvironment,
+            IStringLocalizer<ProductsHomeController> localizer)
+        {
+            _colibriDbContext = colibriDbContext;
+            _hostingEnvironment = hostingEnvironment;
+            _localizer = localizer;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
