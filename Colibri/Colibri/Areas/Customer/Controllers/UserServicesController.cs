89688d8d0077dd092ca881db454479594baffa42
ugorcak <ugorcak@gmx.ch>
2018-11-25 19:41:02 +0100

    extended UserService Controller with the Index ActionMethod, added Index View

--- Colibri/Colibri/Areas/Customer/Controllers/UserServicesController.cs
+++ Colibri/Colibri/Areas/Customer/Controllers/UserServicesController.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Colibri.Data;
+using Colibri.Models;
 using Colibri.Utility;
 using Colibri.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting.Internal;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Localization;
 
 namespace Colibri.Areas.Customer.Controllers
@@ -23,7 +26,7 @@ namespace Colibri.Areas.Customer.Controllers
     {
         private readonly ColibriDbContext _colibriDbContext;
         private readonly HostingEnvironment _hostingEnvironment;
-        private readonly IStringLocalizer<ProductsHomeController> _localizer;
+        private readonly IStringLocalizer<UserServicesController> _localizer;
 
         // bind to the UserServices ViewModel
         // not necessary to create new Objects
@@ -33,16 +36,78 @@ namespace Colibri.Areas.Customer.Controllers
 
         public UserServicesController(ColibriDbContext colibriDbContext, 
             HostingEnvironment hostingEnvironment,
-            IStringLocalizer<ProductsHomeController> localizer)
+            IStringLocalizer<UserServicesController> localizer)
         {
             _colibriDbContext = colibriDbContext;
             _hostingEnvironment = hostingEnvironment;
             _localizer = localizer;
+
+            // initialize the Constructor for the ProductsController
+            UserServicesViewModel = new UserServicesViewModel()
+            {
+                CategoryGroups = _colibriDbContext.CategoryGroups.ToList(),
+                CategoryTypes = _colibriDbContext.CategoryTypes.ToList(),
+                SpecialTags = _colibriDbContext.SpecialTags.ToList(),
+                UserServices = new List<UserServices>(),
+                Users = new List<ApplicationUser>()
+            };
         }
 
-        public IActionResult Index()
+        // Index
+        [Route("Customer/UserServices/Index")]
+        public async Task<IActionResult> Index(
+            string searchUserName = null,
+            string searchServiceName = null)
         {
-            return View();
+            // Filter the Search Criteria
+            StringBuilder param = new StringBuilder();
+
+            param.Append("/Customer/UserService?servicePage=:");
+            param.Append("&searchName=");
+            if (searchUserName != null)
+            {
+                param.Append(searchUserName);
+            }
+            param.Append("&searchName=");
+            if (searchServiceName != null)
+            {
+                param.Append(searchServiceName);
+            }
+
+            // populate the Lists
+            UserServicesViewModel.UserServices = await _colibriDbContext.UserServices.ToListAsync();
+            UserServicesViewModel.Users = from u in _colibriDbContext.ApplicationUsers
+                                                    join p in _colibriDbContext.Products
+                                                    .Include(p => p.ApplicationUser)
+                                                    .ThenInclude(p => p.UserName)
+                                                    on u.Id equals p.ApplicationUserId
+                                                    select u;
+
+            // Search Conditions
+            if (searchUserName != null)
+            {
+                UserServicesViewModel.UserServices = UserServicesViewModel.UserServices
+                    .Where(a => a.ApplicationUser.UserName.ToLower().Contains(searchUserName.ToLower())).ToList();
+            }
+            if (searchServiceName != null)
+            {
+                UserServicesViewModel.UserServices = UserServicesViewModel.UserServices
+                    .Where(a => a.Name.ToLower().Contains(searchServiceName.ToLower())).ToList();
+            }
+
+            // i18n
+            ViewData["UserService"] = _localizer["UserServiceText"];
+            ViewData["UserName"] = _localizer["UserNameText"];
+            ViewData["UserServiceName"] = _localizer["UserServiceNameText"];
+            ViewData["Search"] = _localizer["SearchText"];
+            ViewData["NewUserService"] = _localizer["NewUserServiceText"];
+            ViewData["Price"] = _localizer["PriceText"];
+            ViewData["CategoryGroup"] = _localizer["CategoryGroupText"];
+            ViewData["CategoryType"] = _localizer["CategoryTypeText"];
+            ViewData["Description"] = _localizer["DescriptionText"];
+            ViewData["NumberOfClicks"] = _localizer["NumberOfClicksText"];
+
+            return View(UserServicesViewModel);
         }
     }
 }
