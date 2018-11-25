290482a10b6ac2986ab3ccdb718e63a4bbf8611d
ugorcak <ugorcak@gmx.ch>
2018-11-25 19:12:18 +0100

    added UserServices ViewModel

---
+++ Colibri/Colibri/ViewModels/UserServicesViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Colibri.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.ViewModels
+{
+    public class UserServicesViewModel
+    {
+        public UserServices UserServices { get; set; }
+
+        public IEnumerable<CategoryGroups> CategoryGroups { get; set; }
+        public IEnumerable<CategoryTypes> CategoryTypes { get; set; }
+        public IEnumerable<SpecialTags> SpecialTags { get; set; }
+    }
+}
