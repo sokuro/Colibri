618f0a6d481cd7cf3ab749fa75fcf152184b1bf1
ugorcak <ugorcak@gmx.ch>
2018-12-03 23:39:39 +0100

    added SubscriberViewModel

---
+++ Colibri/Colibri/ViewModels/SubscriberViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Colibri.Models;
+
+namespace Colibri.ViewModels
+{
+    public class SubscriberViewModel
+    {
+        public Notifications Notifications { get; set; }
+
+        //public List<string> MyMessage { get; set; }
+
+        //public List<Notifications> Notifications { get; set; }
+        //public List<string> Notifications { get; set; }
+    }
+}
