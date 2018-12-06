8f5c03ae4be89a8c5b3592c23692b39bfe2dbd8c
ugorcak <ugorcak@gmx.ch>
2018-12-06 22:05:26 +0100

    added UserServicesAddToEntityViewModel to handle Create and Delete Action Methods of the UserServices Controller

---
+++ Colibri/Colibri/ViewModels/UserServicesAddToEntityViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Colibri.Models;
+
+namespace Colibri.ViewModels
+{
+    /*
+     * ViewModel necessary to persist a User Service
+     *
+     * used: Create, Delete
+     */
+    public class UserServicesAddToEntityViewModel
+    {
+        public UserServices UserServices { get; set; }
+        public IEnumerable<CategoryGroups> CategoryGroups { get; set; }
+        public IEnumerable<CategoryTypes> CategoryTypes { get; set; }
+        public IEnumerable<SpecialTags> SpecialTags { get; set; }
+    }
+}
