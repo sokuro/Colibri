a5e63924388eda527515acbbf0ad2e7335dc5450
ugorcak <ugorcak@gmx.ch>
2018-12-08 17:35:52 +0100

    added ApplicationUserCategoryTypesSubscriber to the DB

---
+++ Colibri/Colibri/Data/Migrations/20181208163351_addApplicationUserCategoryTypesSubscriber.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Colibri.Migrations
+{
+    public partial class addApplicationUserCategoryTypesSubscriber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApplicationUserCategoryTypesSubscribers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ApplicationUserId = table.Column<string>(nullable: true),
+                    CategoryTypeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationUserCategoryTypesSubscribers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ApplicationUserCategoryTypesSubscribers_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_ApplicationUserCategoryTypesSubscribers_CategoryTypes_CategoryTypeId",
+                        column: x => x.CategoryTypeId,
+                        principalTable: "CategoryTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationUserCategoryTypesSubscribers_ApplicationUserId",
+                table: "ApplicationUserCategoryTypesSubscribers",
+                column: "ApplicationUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationUserCategoryTypesSubscribers_CategoryTypeId",
+                table: "ApplicationUserCategoryTypesSubscribers",
+                column: "CategoryTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplicationUserCategoryTypesSubscribers");
+        }
+    }
+}
