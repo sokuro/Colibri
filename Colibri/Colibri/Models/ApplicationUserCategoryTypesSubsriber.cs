496237e8b0ed1f755ae4b9f4721d0e2d5d71c868
Kajo <ugorcak@gmx.ch>
2018-12-08 16:43:20 +0100

    added Model for the User and CategoryTypes Notifications

---
+++ Colibri/Colibri/Models/ApplicationUserCategoryTypesSubsriber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colibri.Models
+{
+    /*
+     * Model to bind pesist the n <-> n Relationship between the Application User and the Category Types
+     */
+    public class ApplicationUserCategoryTypesSubsriber
+    {
+        public int Id { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        [Display(Name = "Category Type")]
+        public int CategoryTypeId { get; set; }
+        [ForeignKey("CategoryTypeId")]
+        public virtual CategoryTypes CategoryTypes { get; set; }
+    }
+}
