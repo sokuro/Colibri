97bdcda1686ce8be5a11ab6ec361f7e4c9fd0354
kev kun <kevin.kuenzi1@hotmail.com>
2018-12-08 11:22:23 +0100

    Index-View angepasst

---
+++ Colibri/Colibri/Models/SearchEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Colibri.Models
+{
+    public class SearchEntry
+    {
+        [Required]
+        public int Id { get; set; }
+
+        public string SearchText { get; set; }
+
+        public DateTime SearchDate { get; set; }
+    }
+}
---
+++ Colibri/Colibri/ViewModels/SearchViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Colibri.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Colibri.ViewModels
+{
+    public class SearchViewModel
+    {
+        //public Category Category { get; set; }
+
+        //// Liste mit allen verfügbaren Rubrik-Gruppen
+        //public IEnumerable<CategoryGroup> CategoryGroupList { get; set; }
+
+        //// Liste mit allen Rubriken
+        //public List<string> CategoryList { get; set; }
+
+        //// String StatussMessage wird verwendet, um Fehlermeldungen anzuzeigen
+        //public String StatusMessage { get; set; }
+
+        //public SearchEntry SearchEntry { get; set; }
+
+        //public IEnumerable<Category> CategoriesIEnumerable { get; set; }
+    }
+}
